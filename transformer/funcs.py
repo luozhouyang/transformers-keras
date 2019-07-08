d36a48090067b8737307eb00c089ae3f193ff29a
luozhouyang <zhouyang.luo@gmail.com>
2019-07-08 11:41:51 +0800

    Add masking funcs

--- transformer/funcs.py
+++ transformer/funcs.py
@@ -68,3 +68,34 @@ def positional_encoding(position, d_model):
     pos_encoding = pos_encoding[np.newaxis, ...]
 
     return tf.cast(pos_encoding, dtype=tf.float32)
+
+
+def create_padding_mask(seq):
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+
+    # add extra dimensions so that we can add the padding
+    # to the attention logits.
+    return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, 1, seq_len)
+
+
+def create_look_ahead_mask(size):
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    return mask  # (seq_len, seq_len)
+
+
+def create_masks(inp, tar):
+    # Encoder padding mask
+    enc_padding_mask = create_padding_mask(inp)
+
+    # Used in the 2nd attention block in the decoder.
+    # This padding mask is used to mask the encoder outputs.
+    dec_padding_mask = create_padding_mask(inp)
+
+    # Used in the 1st attention block in the decoder.
+    # It is used to pad and mask future tokens in the input received by
+    # the decoder.
+    look_ahead_mask = create_look_ahead_mask(tf.shape(tar)[1])
+    dec_target_padding_mask = create_padding_mask(tar)
+    combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
+
+    return enc_padding_mask, combined_mask, dec_padding_mask
--- transformer/funcs_test.py
+++ transformer/funcs_test.py
@@ -38,6 +38,16 @@ class FuncsTest(tf.test.TestCase):
         temp_q = tf.constant([[0, 0, 10], [0, 10, 0], [10, 10, 0]], dtype=tf.float32)  # (3, 3)
         funcs.print_scaled_dot_product_attention_out(temp_q, temp_k, temp_v)
 
+        inp = tf.constant([
+            [1, 2, 3, 4],
+            [3, 4, 5, 6]
+        ])
+        tar = inp
+        enc_padding_mask, combined_mask, dec_padding_mask = funcs.create_masks(inp, tar)
+        print(enc_padding_mask.shape)
+        print(combined_mask.shape)
+        print(dec_padding_mask.shape)
+
 
 if __name__ == '__main__':
     tf.test.main()
