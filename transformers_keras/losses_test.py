4db888e6e5720f891a17f045bbfbd33fa6bafcf3
luozhouyang <zhouyang.luo@gmail.com>
2020-03-05 15:17:12 +0800

    Add assertion

--- transformers_keras/losses_test.py
+++ transformers_keras/losses_test.py
@@ -35,10 +35,14 @@ class LossesTest(tf.test.TestCase):
             shape=(2, 3, 5),
             dtype=tf.float32
         )
-        # the last two time steps is masking positions, so these tow losses should be equal
+        # the last two time steps is masking positions, so these two losses should be equal
         loss0 = masked_sparse_categorical_crossentropy(x, y0)
         loss1 = masked_sparse_categorical_crossentropy(x, y1)
         self.assertEqual(loss0, loss1)
+
+        # the first 4 positions are the same, so masked ce and ce should be almost equal(masked ce uses epsilon=1e-6)
+        ce = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)(x, y0)
+        self.assertAlmostEqual(ce.numpy(), loss0.numpy(), delta=6)
 
 
 if __name__ == "__main__":
