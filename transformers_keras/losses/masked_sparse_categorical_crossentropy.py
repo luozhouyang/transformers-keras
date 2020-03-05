eb071a6f8008e7350c8c8bb86bf90367786b5b83
luozhouyang <zhouyang.luo@gmail.com>
2020-03-05 14:10:43 +0800

    Add mask_id argument

--- transformers_keras/losses/masked_sparse_categorical_crossentropy.py
+++ transformers_keras/losses/masked_sparse_categorical_crossentropy.py
@@ -3,20 +3,20 @@ from tensorflow.python.keras.losses import LossFunctionWrapper
 from tensorflow.python.keras.utils import losses_utils
 
 
-def masked_sparse_categorical_crossentropy(y_true, y_pred, from_logits=False, axis=-1):
+def masked_sparse_categorical_crossentropy(y_true, y_pred, from_logits=False, mask_id=0):
     """Loss for masked language model.
 
     Args:
         y_true: Tensor, shape (batch_size, time_steps)
         y_pred: Tensor, shape (batch_size, tiem_steps, vocab_size)
-
+        from_logits: Python boolean, whether `y_pred` is logits or not.
+        mask_id: Python integer, id from `y_true` that to be masked.
     Returns:
         loss: Scalar.
     """
     loss = tf.keras.losses.sparse_categorical_crossentropy(
-        y_true, y_pred, from_logits=from_logits, axis=axis)  # shape (batch_size, time_steps)
-    # 0 -> padding token id, masking padding tokens
-    mask = tf.logical_not(tf.equal(y_true, 0))  # shape (batch_size, time_steps)
+        y_true, y_pred, from_logits=from_logits, axis=-1)  # shape (batch_size, time_steps)
+    mask = tf.logical_not(tf.equal(y_true, mask_id))  # shape (batch_size, time_steps)
     mask = tf.cast(mask, dtype=loss.dtype)
     loss *= mask  # shape (batch_size, time_steps)
     return loss  # do not reduce here!
@@ -28,10 +28,12 @@ class MaskedSparseCategoricalCrossentropy(LossFunctionWrapper):
     def __init__(self,
                  from_logits=False,
                  reduction=losses_utils.ReductionV2.AUTO,
+                 mask_id=0,
                  name='masked_sparse_categorical_crossentropy'):
         super(MaskedSparseCategoricalCrossentropy, self).__init__(
             masked_sparse_categorical_crossentropy,
             name=name,
             reduction=reduction,
-            from_logits=from_logits
+            from_logits=from_logits,
+            mask_id=mask_id
         )
--- transformers_keras/losses/masked_sparse_categorical_crossentropy_test.py
+++ transformers_keras/losses/masked_sparse_categorical_crossentropy_test.py
@@ -7,7 +7,7 @@ from .masked_sparse_categorical_crossentropy import \
 class MaskedSparseCategoricalCrossentropyTest(tf.test.TestCase):
 
     def testMaskedSparseCategoricalCrossentropy(self):
-        ce = MaskedSparseCategoricalCrossentropy(from_logits=False)
+        ce = MaskedSparseCategoricalCrossentropy(from_logits=True, mask_id=5)
         x = tf.constant([1, 2, 3, 4, 5, 6], shape=(2, 3), dtype=tf.int32)
         y = tf.random.uniform(shape=(2, 3, 8), dtype=tf.float32)
         y = tf.nn.softmax(y)
