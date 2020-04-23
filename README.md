b45e8350706eebbe5107c12247e2122b7f823a0b
luozhouyang <zhouyang.luo@gmail.com>
2020-04-23 16:12:09 +0800

    Inflate run bert script

--- README.md
+++ README.md
@@ -5,15 +5,25 @@ Transformer-based models implemented in tensorflow 2.x(Keras).
 ## Models
 
 - [x] Transformer
-    * [Attention Is All You Need](https://arxiv.org/abs/1706.03762). 
-    * Here is a tutorial from tensorflow:[Transformer model for language understanding](https://www.tensorflow.org/beta/tutorials/text/transformer)
-- [ ] TransformerXL
-- [ ] GPT
-- [ ] GPT2
-- [ ] BERT
-- [ ] XLNet
+  * [Attention Is All You Need](https://arxiv.org/abs/1706.03762). 
+  * Here is a tutorial from tensorflow:[Transformer model for language understanding](https://www.tensorflow.org/beta/tutorials/text/transformer)
+- [x] BERT
+  * [BERT: Pre-training of Deep Bidirectional Transformers for Language Understanding](https://arxiv.org/abs/1810.04805)
+- [ ] ALBERT
+  * [ALBERT: A Lite BERT for Self-supervised Learning of Language Representations](https://arxiv.org/abs/1909.11942)
 
 
-## Documents
 
-* [Transformer](doc/tramsformer.md)
+## BERT
+
+Use your own data to pretrain a BERT model.
+
+```bash
+python -m transformers_keras.run_bert \
+    --mode train \
+    --model_config config/bert_model_config.json \
+    --data_config config/bert_data_config.json
+```
+
+> Tips:
+> You need prepare your data to tfrecord format. You can use this script: [create_pretraining_data.py](https://github.com/google-research/bert/blob/master/create_pretraining_data.py)
--- transformers_keras/run_bert.py
+++ transformers_keras/run_bert.py
@@ -1,8 +1,18 @@
+import argparse
+import json
+import logging
+import os
+
 import tensorflow as tf
+
+from transformers_keras.callbacks.model_step_checkpoint import \
+    ModelStepCheckpoint
+from transformers_keras.callbacks.saved_model_exporter import \
+    SavedModelExporter
 
 from .losses import MaskedSparseCategoricalCrossentropy
 from .metrics import MaskedSparseCategoricalAccuracy
-from .modeling_bert import *
+from .modeling_bert import Bert4PreTraining, BertConfig
 
 
 def build_model(config):
@@ -35,8 +45,143 @@ def build_model(config):
     return model
 
 
+class BertDataConfig(object):
+
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.train_batch_size = kwargs.pop('train_batch_size', 32)
+        self.train_record_files = kwargs.pop('train_record_files', None)
+        self.valid_batch_size = kwargs.pop('valid_batch_size', 32)
+        self.valid_record_files = kwargs.pop('valid_record_files', None)
+        self.predict_batch_size = kwargs.pop('predict_batch_size', 1)
+        self.shuffle_buffer_size = kwargs.pop('shuffle_buffer_size', 1000000)
+        self.shuffle_seed = kwargs.pop('shuffle_seed', None)
+        self.reshuffle_each_iteration = kwargs.pop('reshuffle_each_iteration', True)
+        self.drop_remainder = kwargs.pop('drop_remainder', True)
+        self.epochs = kwargs.pop('epochs', 10)
+        self.model_dir = kwargs.pop('model_dir', '/tmp/bert')
+
+
+class BertDataset(object):
+
+    def __init__(self, config, **kwargs):
+        self.config = config
+
+    def _parse_example_fn(self, record):
+        raise NotImplementedError()
+
+    def build_train_dataset(self, train_record_files):
+        dataset = tf.data.TFRecordDataset(train_record_files)
+        dataset = dataset.map(self._parse_example_fn, num_parallels=tf.data.experimental.AUTOTUNE)
+        dataset = dataset.shuffle(
+            buffer_size=self.config.shuffle_buffer_size,
+            seed=self.config.shuffle_seed,
+            reshuffle_each_iteration=self.config.reshuffle_each_iteration)
+        dataset = dataset.batch(
+            self.config.train_batch_size, drop_remainder=self.config.drop_remainder
+        ).prefetch(self.config.train_batch_size)
+        return dataset
+
+    def build_valid_dataset(self, valid_record_files):
+        if valid_record_files is None:
+            return None
+        dataset = tf.data.TFRecordDataset(valid_record_files)
+        dataset = dataset.map(self._parse_example_fn, num_parallels=tf.data.experimental.AUTOTUNE)
+        dataset = dataset.batch(self.config.valid_batch_size).prefetch(self.config.valid_batch_size)
+        return dataset
+
+    def build_predict_dataset(self, predict_record_files):
+        dataset = tf.data.TFRecordDataset(predict_record_files)
+        dataset = dataset.map(self._parse_example_fn, num_parallels=tf.data.experimental.AUTOTUNE)
+        dataset = dataset.batch(self.config.predict_batch_size).prefetch(self.config.predict_batch_size)
+        return dataset
+
+
+class CustomBertDataset(BertDataset):
+
+    def _parse_example_fn(self, record):
+        """Parse tfrecord to example. Change this parse process according to your record files format."""
+        MAX_SEQ_LEN = 512
+        MAX_PREDICTIONS_PER_SEQ = 512
+        name_to_features = {
+            'input_ids': tf.io.FixedLenFeature([MAX_SEQ_LEN], tf.int64),
+            'input_mask': tf.io.FixedLenFeature([MAX_SEQ_LEN], tf.int64),
+            'segment_ids': tf.io.FixedLenFeature([MAX_SEQ_LEN], tf.int64),
+            'masked_lm_positions': tf.io.FixedLenFeature([MAX_PREDICTIONS_PER_SEQ], tf.int64),
+            'masked_lm_ids': tf.io.FixedLenFeature([MAX_PREDICTIONS_PER_SEQ], tf.int64),
+            'masked_lm_weights': tf.io.FixedLenFeature([MAX_PREDICTIONS_PER_SEQ], tf.float32),
+            'next_sentence_labels': tf.io.FixedLenFeature([1], tf.int64),
+        }
+
+        example = tf.io.parse_single_example(record, name_to_features)
+        # tf.Example only supports tf.int64, but the TPU only supports tf.int32.
+        # So cast all int64 to int32.
+        for name in list(example.keys()):
+            t = example[name]
+            if t.dtype == tf.int64:
+                t = tf.cast(t, tf.int32)
+            example[name] = t
+
+        # extract needed values from example to matching network's inputs and outputs
+        features = {
+            'input_ids': example.get('input_ids', None),
+            'input_mask': example.get('input_mask', None),
+            'segment_ids': example.get('segment_ids', None),
+        }
+        labels = {
+            'predictions': example.get('masked_lm_ids', None),
+            'relations': tf.one_hot(example['next_sentence_lables'], 2)
+        }
+        return (features, labels)
+
+
+def train(model, dataconfig):
+    dataset = CustomBertDataset(dataconfig)
+    train_dataset = dataset.build_train_dataset(dataconfig.train_record_files)
+    valid_dataset = dataset.build_valid_dataset(dataconfig.valid_record_files)
+
+    model_dir = dataconfig.model_dir
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+    ckpt_path = model_dir + '/' + 'bert-{epoch:04d}.ckpt'
+    model.fit(
+        train_dataset,
+        validation_data=valid_dataset,
+        validation_steps=1000,
+        epochs=dataconfig.epochs,
+        callbacks=[
+            tf.keras.callbacks.EarlyStopping(monitor='loss', patience=5),
+            tf.keras.callbacks.TensorBoard(os.path.join(model_dir, 'tensorboard'), update_freq='batch'),
+            ModelStepCheckpoint(model, model_dir, every_steps=10000, max_keep_ckpt=10),
+            SavedModelExporter(model, os.path.join(model_dir, 'export'), every_steps=100000)
+        ]
+    )
+
+
 if __name__ == "__main__":
-    config = BertConfig()
-    model = build_model(config)
-    model.summary()
-    tf.keras.utils.plot_model(model, 'model.png', show_shapes=True, expand_nested=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', type=str, default='train')
+    parser.add_argument('--model_config', type=str, help="Model config file in JSON format.")
+    parser.add_argument('--data_config', type=str, help="Data config file in JSON format.")
+
+    args, _ = parser.parse_known_args()
+
+    model_config, data_config = {}, {}
+    if os.path.exists(args.model_config):
+        logging.info('Load model config from: %s.' % args.model_config)
+        with open(args.model_config, mode='rt', encoding='utf8') as f:
+            model_config = json.load(f)
+    if os.path.exists(args.data_config):
+        logging.info('Load data config from: %s.' % args.data_config)
+        with open(args.data_config, mode='rt', encoding='utf8') as f:
+            data_config = json.load(f)
+
+    bert_model_config = BertConfig(**model_config)
+    bert_data_config = BertDataConfig(**data_config)
+    model = build_model(bert_model_config)
+
+    if 'train' == args.mode:
+        train(model, bert_data_config)
+
+    else:
+        raise ValueError('Invalid `mode` argument.')
