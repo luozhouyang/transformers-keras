3335ecb004e9e263193bbbb7418af609b6893857
luozhouyang <zhouyang.luo@gmail.com>
2020-06-05 17:57:12 +0800

    Update log format

--- transformers_keras/__init__.py
+++ transformers_keras/__init__.py
@@ -1,4 +1,7 @@
 import logging
 
-logging.basicConfig(format="%(filename)s - %(asctime)s - %(message)s", level=logging.INFO)
+__name__ = 'transformers_keras'
+__version__ = '0.0.1'
+
+logging.basicConfig(format="%(asctime)s %(levelname)s %(filename)20s %(lineno)4d] %(message)s", level=logging.INFO)
 logging.info("Init logging...")
