015b4d506f3e1fa47c1a0f0dd46144ff09441901
luozhouyang <zhouyang.luo@gmail.com>
2020-06-05 17:02:47 +0800

    Extract common layers

---
+++ transformers_keras/layers.py
@@ -0,0 +1,179 @@
+import tensorflow as tf
+
+
+class ScaledDotProductAttention(tf.keras.layers.Layer):
+
+    def __init__(self, **kwargs):
+        super(ScaledDotProductAttention, self).__init__(**kwargs)
+
+    def call(self, inputs, training=None):
+        query, key, value, mask = inputs
+        score = tf.matmul(query, key, transpose_b=True)
+        dk = tf.cast(tf.shape(query)[-1], tf.float32)
+        score = score / tf.math.sqrt(dk)
+        if mask is not None:
+            score += mask * -10000.0
+        attn_weights = tf.nn.softmax(score, axis=1)
+        context = tf.matmul(attn_weights, value)
+        return context, attn_weights
+
+    def get_config(self):
+        return super().get_config()
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+
+    def __init__(self, hidden_size=512, num_attention_heads=8, **kwargs):
+        super(MultiHeadAttention, self).__init__(**kwargs)
+        self.hidden_size = hidden_size
+        self.num_attention_heads = num_attention_heads
+        assert self.hidden_size % self.num_attention_heads == 0
+
+        self.query_weight = tf.keras.layers.Dense(self.hidden_size)
+        self.key_weight = tf.keras.layers.Dense(self.hidden_size)
+        self.value_weight = tf.keras.layers.Dense(self.hidden_size)
+
+        self.attention = ScaledDotProductAttention()
+
+        self.dense = tf.keras.layers.Dense(self.hidden_size)
+
+    def _split_heads(self, x, batch_size):
+        x = tf.reshape(x, (batch_size, -1, self.num_attention_heads, self.hidden_size // self.num_attention_heads))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, inputs, training=None):
+        query, key, value, mask = inputs
+        batch_size = tf.shape(query)[0]
+
+        query = self._split_heads(self.query_weight(query), batch_size)
+        key = self._split_heads(self.key_weight(key), batch_size)
+        value = self._split_heads(self.value_weight(value), batch_size)
+
+        context, attn_weights = self.attention(inputs=(query, key, value, mask))
+        context = tf.transpose(context, perm=[0, 2, 1, 3])
+        context = tf.reshape(context, [batch_size, -1, self.hidden_size])
+        output = self.dense(context)
+        return output, attn_weights
+
+    def get_config(self):
+        config = {
+            'hidden_size': self.hidden_size,
+            'num_attention_heads': self.num_attention_heads
+        }
+        base = super().get_config()
+        return dict(list(base.items()) + list(config.items()))
+
+
+class PairWiseFeedForwardNetwork(tf.keras.layers.Layer):
+
+    def __init__(self, hidden_size=512, ffn_size=2048, **kwargs):
+        super(PairWiseFeedForwardNetwork, self).__init__(**kwargs)
+        self.ffn_size = ffn_size
+        self.hidden_size = hidden_size
+        self.dense1 = tf.keras.layers.Dense(self.ffn_size, activation='relu')
+        self.dense2 = tf.keras.layers.Dense(self.hidden_size)
+
+    def call(self, inputs, training=None):
+        outputs = self.dense2(self.dense1(inputs))
+        return outputs
+
+    def get_config(self):
+        config = {
+            'ffn_size': self.ffn_size,
+            'hidden_size': self.hidden_size,
+        }
+        p = super(PairWiseFeedForwardNetwork, self).get_config()
+        return dict(list(p.items()) + list(config.items()))
+
+
+class EncoderLayer(tf.keras.layers.Layer):
+
+    def __init__(self, hidden_size=512, num_attention_heads=8, ffn_size=2048, dropout_rate=0.2, epsilon=1e-6, **kwargs):
+        super(EncoderLayer, self).__init__(**kwargs)
+        self.hidden_size = hidden_size
+        self.num_attention_heads = num_attention_heads
+        self.ffn_size = ffn_size
+        self.dropout_rate = dropout_rate
+        self.epsilon = epsilon
+
+        self.attention = MultiHeadAttention(self.hidden_size, self.num_attention_heads)
+        self.attn_dropout = tf.keras.layers.Dropout(self.dropout_rate)
+        self.attn_layer_norm = tf.keras.layers.LayerNormalization(epsilon=self.epsilon)
+
+        self.ffn = PairWiseFeedForwardNetwork(self.hidden_size, self.ffn_size)
+        self.ffn_dropout = tf.keras.layers.Dropout(self.dropout_rate)
+        self.ffn_layer_norm = tf.keras.layers.LayerNormalization(epsilon=self.epsilon)
+
+    def call(self, inputs, training=None):
+        query, key, value, mask = inputs
+        attn, attn_weights = self.attention(inputs=(query, key, value, mask))
+        attn = self.attn_dropout(attn, training=training)
+        attn = self.attn_layer_norm(query + attn)
+
+        ffn = self.ffn(attn)
+        ffn = self.ffn_dropout(ffn, training=training)
+        ffn = self.ffn_layer_norm(ffn + attn)
+
+        return ffn, attn_weights
+
+    def get_config(self):
+        config = {
+            'hidden_size': self.hidden_size,
+            'num_attention_heads': self.num_attention_heads,
+            'ffn_size': self.ffn_size,
+            'dropout_rate': self.dropout_rate,
+            'epsilon': self.epsilon
+        }
+        base = super().get_config()
+        return dict(list(base.items()) + list(config.items()))
+
+
+class DecoderLayer(tf.keras.layers.Layer):
+
+    def __init__(self, hidden_size=512, num_attention_heads=8, ffn_size=2048, dropout_rate=0.2, epsilon=1e-6, **kwargs):
+        super(DecoderLayer, self).__init__(**kwargs)
+        self.hidden_size = hidden_size
+        self.num_attention_heads = num_attention_heads
+        self.ffn_size = ffn_size
+        self.dropout_rate = dropout_rate
+        self.epsilon = epsilon
+
+        self.self_attention = MultiHeadAttention(self.hidden_size, self.num_attention_heads)
+        self.self_attn_dropout = tf.keras.layers.Dropout(self.dropout_rate)
+        self.self_attn_layer_norm = tf.keras.layers.LayerNormalization(epsilon=self.epsilon)
+
+        self.context_attention = MultiHeadAttention(self.hidden_size, self.num_attention_heads)
+        self.context_attn_dropout = tf.keras.layers.Dropout(self.dropout_rate)
+        self.context_attn_layer_norm = tf.keras.layers.LayerNormalization(epsilon=self.epsilon)
+
+        self.ffn = PairWiseFeedForwardNetwork(self.hidden_size, self.ffn_size)
+        self.ffn_dropout = tf.keras.layers.Dropout(self.dropout_rate)
+        self.ffn_layer_norm = tf.keras.layers.LayerNormalization(epsilon=self.epsilon)
+
+    def call(self, inputs, training=None):
+        x, enc_outputs, look_ahead_mask, padding_mask = inputs
+
+        attn1, attn1_weights = self.self_attention(inputs=(x, x, x, look_ahead_mask))
+        attn1 = self.self_attn_dropout(attn1, training=training)
+        output1 = self.self_attn_layer_norm(attn1 + x)
+
+        attn2, attn2_weights = self.context_attention(inputs=(output1, enc_outputs, enc_outputs, padding_mask))
+        attn2 = self.context_attn_dropout(attn2, training=training)
+        output2 = self.context_attn_layer_norm(attn2 + output1)
+
+        ffn = self.ffn(output2)
+        ffn = self.ffn_dropout(ffn, training=training)
+        ffn = self.ffn_layer_norm(ffn + attn2)
+
+        return ffn, attn1_weights, attn2_weights
+
+    def get_config(self):
+        config = {
+            'hidden_size': self.hidden_size,
+            'num_attention_heads': self.num_attention_heads,
+            'ffn_size': self.ffn_size,
+            'dropout_rate': self.dropout_rate,
+            'epsilon': self.epsilon
+        }
+        base = super(DecoderLayer, self).get_config()
+        return dict(list(base.items()) + list(config.items()))
---
+++ transformers_keras/layers_test.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+
+from .layers import *
+
+
+def _create_padding_mask(x):
+    mask = tf.cast(tf.equal(0, x), dtype=tf.float32)
+    mask = mask[:, tf.newaxis, tf.newaxis, :]
+    return mask
+
+
+def _create_look_ahead_mask(size):
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    return mask
+
+
+def _create_masks(x, y):
+    _enc_padding_mask = _create_padding_mask(x)
+    _dec_padding_mask = _create_padding_mask(x)
+    _look_ahead_mask = _create_look_ahead_mask(tf.shape(y)[1])
+    _target_padding_mask = _create_padding_mask(y)
+    combined = tf.maximum(_look_ahead_mask, _target_padding_mask)
+    return _enc_padding_mask, combined, _dec_padding_mask
+
+
+class LayersTest(tf.test.TestCase):
+
+    def testScaledDotProductAttention(self):
+        attention = ScaledDotProductAttention()
+        x = tf.random.uniform(shape=(4, 16, 64))
+        mask = tf.cast(tf.linalg.band_part(
+            tf.ones(shape=(4, 16, 16)), num_lower=0, num_upper=-1), dtype=tf.float32)
+        context, attn_weight = attention(inputs=(x, x, x, mask), training=False)
+        self.assertEqual([4, 16, 64], context.shape)
+        self.assertEqual([4, 16, 16], attn_weight.shape)
+
+    def testMultiHeadAttention(self):
+        attention = MultiHeadAttention(hidden_size=512, num_attention_heads=8)
+        x = tf.random.uniform(shape=(4, 16, 512))
+        mask = tf.cast(tf.linalg.band_part(
+            tf.ones(shape=(4, 1, 16, 16)), num_lower=0, num_upper=-1), dtype=tf.float32)
+        context, attn_weight = attention(inputs=(x, x, x, mask), training=False)
+        self.assertEqual([4, 16, 512], context.shape)
+        self.assertEqual([4, 8, 16, 16], attn_weight.shape)
+
+    def testEncoderLayer(self):
+        encoder_layer = EncoderLayer(hidden_size=512, num_attention_heads=8, ffn_size=2048)
+        x = tf.random.uniform(shape=(2, 5, 512))
+        padding_mask = tf.constant([[0, 0, 0, 1, 1], [0, 0, 0, 0, 1]], shape=(2, 5), dtype=tf.float32)
+        padding_mask = padding_mask[:, tf.newaxis, tf.newaxis, :]
+
+        outputs, attn_weights = encoder_layer(inputs=(x, x, x, padding_mask))
+        self.assertEqual([2, 5, 512], outputs.shape)
+        self.assertEqual([2, 8, 5, 5], attn_weights.shape)
+
+    def testDecoderLayer(self):
+        decoder_layer = DecoderLayer(hidden_size=512, num_attention_heads=8, ffn_size=2048)
+        x = tf.constant([[1, 2, 3, 4, 0], [2, 3, 3, 0, 0]], shape=(2, 5), dtype=tf.int64)
+        y = tf.constant([[1, 2, 3, 4, 1, 0, 0], [2, 3, 3, 4, 5, 6, 7]], shape=(2, 7), dtype=tf.int64)
+
+        _, look_ahead_mask, padding_mask = _create_masks(x, y)
+
+        enc_outputs = tf.random.uniform(shape=(2, 5, 512))
+
+        dec_inputs = tf.random.uniform((2, 7, 512))
+        dec_outputs, attn_weights1, attn_weights2 = decoder_layer(inputs=(dec_inputs, enc_outputs, look_ahead_mask, padding_mask))
+
+        self.assertEqual([2, 7, 512], dec_outputs.shape)
+        self.assertEqual([2, 8, 7, 7], attn_weights1.shape)
+        self.assertEqual([2, 8, 7, 5], attn_weights2.shape)
+
+
+if __name__ == "__main__":
+    tf.test.main()
