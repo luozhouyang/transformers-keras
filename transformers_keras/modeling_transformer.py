ef6238aca200cf70b4137126543d4013737f7bf6
luozhouyang <zhouyang.luo@gmail.com>
2020-06-05 17:06:30 +0800

    Refactoring transformer

--- transformers_keras/modeling_transformer.py
+++ transformers_keras/modeling_transformer.py
@@ -1,5 +1,6 @@
 import numpy as np
-import tensorflow as tf
+
+from .layers import *
 
 
 class TransformerConfig(object):
@@ -14,6 +15,7 @@ class TransformerConfig(object):
         self.source_vocab_size = kwargs.pop('source_vocab_size', 100)
         self.target_vocab_size = kwargs.pop('target_vocab_size', 100)
         self.max_positions = kwargs.pop('max_positions', 512)
+        self.epsilon = kwargs.get('epsilon', 1e-6)
 
 
 class TransformerEmbedding(tf.keras.layers.Layer):
@@ -28,7 +30,6 @@ class TransformerEmbedding(tf.keras.layers.Layer):
         self.token_embedding = tf.keras.layers.Embedding(self.source_vocab_size, self.hidden_size)
 
     def build(self, input_shape):
-
         def _initializer(shape, dtype=tf.float32):
             pos = np.arange(self.max_positions)[:, tf.newaxis]
             d = np.arange(self.hidden_size)[tf.newaxis, :]
@@ -73,124 +74,6 @@ class TransformerEmbedding(tf.keras.layers.Layer):
         return dict(list(p.items()) + list(conf.items()))
 
 
-class MultiHeadAttention(tf.keras.layers.Layer):
-
-    def __init__(self, config, **kwargs):
-        super(MultiHeadAttention, self).__init__(**kwargs)
-        self.num_attention_heads = config.num_attention_heads
-        self.hidden_size = config.hidden_size
-        assert self.hidden_size % self.num_attention_heads == 0
-        self.depth = self.hidden_size // self.num_attention_heads
-
-        self.wq = tf.keras.layers.Dense(self.hidden_size)
-        self.wk = tf.keras.layers.Dense(self.hidden_size)
-        self.wv = tf.keras.layers.Dense(self.hidden_size)
-
-        self.dropout_rate = config.dropout_rate
-        self.attention_dropout = tf.keras.layers.Dropout(self.dropout_rate)
-        self.dense = tf.keras.layers.Dense(self.hidden_size)
-
-        self.dropout = tf.keras.layers.Dropout(self.dropout_rate)
-        self.layer_norm = tf.keras.layers.LayerNormalization(epsilon=1e-12)
-
-    def call(self, inputs, attn_mask=None, training=None):
-        query, key, value = inputs
-        residual = query
-        batch_size = tf.shape(query)[0]
-
-        def _split_heads(x):
-            x = tf.reshape(x, (batch_size, -1, self.num_attention_heads, self.depth))
-            return tf.transpose(x, perm=[0, 2, 1, 3])
-
-        query = _split_heads(self.wq(query))
-        key = _split_heads(self.wk(key))
-        value = _split_heads(self.wv(value))
-
-        def _scaled_dot_product_attention(q, k, v, mask=None):
-            s = tf.matmul(q, k, transpose_b=True)
-            dk = tf.cast(tf.shape(q)[-1], dtype=tf.float32)
-            s = s/tf.math.sqrt(dk)
-            if mask is not None:
-                s += mask * -10000.0
-            attn = tf.nn.softmax(s)
-            attn = self.attention_dropout(attn, training=training)
-            context = tf.matmul(attn, v)
-            context = tf.transpose(context, perm=[0, 2, 1, 3])
-            context = tf.reshape(context, shape=(batch_size, -1, self.hidden_size))
-            return context, attn
-
-        context, attn_weights = _scaled_dot_product_attention(query, key, value, attn_mask)
-        outputs = self.dense(context)
-        outputs = self.dropout(outputs, training=training)
-        outputs = self.layer_norm(residual + outputs)
-        return outputs, attn_weights
-
-    def get_config(self):
-        config = {
-            'num_attention_heads': self.num_attention_heads,
-            'hidden_size': self.hidden_size,
-            'depth': self.depth,
-            'dropout_rate': self.dropout_rate,
-        }
-        p = super(MultiHeadAttention, self).get_config()
-        return dict(list(p.items()) + list(config.items()))
-
-
-class PairWiseFeedForwardNetwork(tf.keras.layers.Layer):
-
-    def __init__(self, config, **kwargs):
-        super(PairWiseFeedForwardNetwork, self).__init__(**kwargs)
-        self.ffn_size = config.ffn_size
-        self.hidden_size = config.hidden_size
-        self.dropout_rate = config.dropout_rate
-        self.dense1 = tf.keras.layers.Dense(self.ffn_size, activation='relu')
-        self.dense2 = tf.keras.layers.Dense(self.hidden_size)
-        self.dropout = tf.keras.layers.Dropout(self.dropout_rate)
-        self.layer_norm = tf.keras.layers.LayerNormalization(epsilon=1e-12)
-
-    def call(self, inputs, training=None):
-        outputs = self.dense2(self.dense1(inputs))
-        outputs = self.dropout(outputs, training=training)
-        outputs = self.layer_norm(outputs + inputs)
-        return outputs
-
-    def get_config(self):
-        config = {
-            'ffn_size': self.ffn_size,
-            'hidden_size': self.hidden_size,
-        }
-        p = super(PairWiseFeedForwardNetwork, self).get_config()
-        return dict(list(p.items()) + list(config.items()))
-
-
-class TransformerEncoderLayer(tf.keras.layers.Layer):
-
-    def __init__(self, config, **kwargs):
-        super(TransformerEncoderLayer, self).__init__(**kwargs)
-        self.hidden_size = config.hidden_size
-        self.num_attention_heads = config.num_attention_heads
-        self.ffn_size = config.ffn_size
-        self.dropout_rate = config.dropout_rate
-
-        self.mha = MultiHeadAttention(config)
-        self.ffn = PairWiseFeedForwardNetwork(config)
-
-    def call(self, inputs, attn_mask=None):
-        hidden_states, attn_weights = self.mha(inputs=inputs, attn_mask=attn_mask)
-        outputs = self.ffn(hidden_states)
-        return outputs, attn_weights
-
-    def get_config(self):
-        config = {
-            'hidden_size': self.hidden_size,
-            'num_attention_heads': self.num_attention_heads,
-            'ffn_size': self.ffn_size,
-            'dropout_rate': self.dropout_rate
-        }
-        p = super(TransformerEncoderLayer, self).get_config()
-        return dict(list(p.items()) + list(config.items()))
-
-
 class TransformerEncoder(tf.keras.layers.Layer):
 
     def __init__(self, config, **kwargs):
@@ -198,23 +81,25 @@ class TransformerEncoder(tf.keras.layers.Layer):
         self.num_layers = config.num_encoder_layers
         self.embedding = TransformerEmbedding(config)
         self.encoders = [
-            TransformerEncoderLayer(config) for _ in range(self.num_layers)
+            EncoderLayer(
+                hidden_size=config.hidden_size,
+                num_attention_heads=config.num_attention_heads,
+                ffn_size=config.ffn_size,
+                dropout_rate=config.dropout_rate,
+                epsilon=config.epsilon,
+                name='encoder_layer_{}'.format(i)
+            ) for i in range(self.num_layers)
         ]
 
-    def call(self, inputs):
-        token_ids = inputs
+    def call(self, inputs, training=None):
+        token_ids, mask = inputs
         embeddings = self.embedding(inputs=token_ids)
-
-        def _create_mask(x):
-            mask = tf.cast(tf.equal(0, x), dtype=tf.float32)
-            mask = mask[:, tf.newaxis, tf.newaxis, :]
-            return mask
 
         attn_weights = []
         outputs = embeddings
-        attn_mask = _create_mask(token_ids)
         for i in range(self.num_layers):
-            outputs, weights = self.encoders[i](inputs=(outputs, outputs, outputs), attn_mask=attn_mask)
+            encoder = self.encoders[i]
+            outputs, weights = encoder(inputs=(outputs, outputs, outputs, mask))
             attn_weights.append(weights)
 
         return outputs, attn_weights
@@ -227,36 +112,6 @@ class TransformerEncoder(tf.keras.layers.Layer):
         return dict(list(p.items()) + list(conf.items()))
 
 
-class TransformerDecoderLayer(tf.keras.layers.Layer):
-
-    def __init__(self, config, **kwargs):
-        super(TransformerDecoderLayer, self).__init__(name='TransformerDecoderLayer', **kwargs)
-        self.mha1 = MultiHeadAttention(config)
-        self.mha2 = MultiHeadAttention(config)
-        self.ffn = PairWiseFeedForwardNetwork(config)
-        self.dropout_rate = config.dropout_rate
-        self.dropout = tf.keras.layers.Dropout(self.dropout_rate)
-        self.layer_norm = tf.keras.layers.LayerNormalization(epsilon=1e-12)
-
-    def call(self, inputs, look_ahead_mask=None, padding_mask=None, training=None):
-        x, enc_output = inputs
-        dec_outputs_1, self_attention_weights = self.mha1(inputs=(x, x, x), attn_mask=look_ahead_mask)
-        dec_outputs_2, context_attention_weights = self.mha2(
-            inputs=(enc_output, enc_output, dec_outputs_1), attn_mask=padding_mask)
-
-        outputs = self.ffn(dec_outputs_2)
-        outputs = self.dropout(outputs, training=training)
-        outputs = self.layer_norm(outputs + dec_outputs_2)
-        return outputs, self_attention_weights, context_attention_weights
-
-    def get_config(self):
-        config = {
-            'dropout_rate': self.dropout_rate,
-        }
-        p = super(TransformerDecoderLayer, self).get_config()
-        return dict(list(p.items()) + list(config.items()))
-
-
 class TransformerDecoder(tf.keras.layers.Layer):
 
     def __init__(self, config, **kwargs):
@@ -265,27 +120,26 @@ class TransformerDecoder(tf.keras.layers.Layer):
         self.hidden_size = config.hidden_size
         self.embedding = TransformerEmbedding(config)
         self.decoders = [
-            TransformerDecoderLayer(config) for _ in range(self.num_layers)
+            DecoderLayer(
+                hidden_size=config.hidden_size,
+                num_attention_heads=config.num_attention_heads,
+                ffn_size=config.ffn_size,
+                dropout_rate=config.dropout_rate,
+                epsilon=config.epsilon,
+                name='decoder_layer_{}'.format(i)
+            ) for i in range(self.num_layers)
         ]
 
-    def call(self, inputs):
-        token_ids, enc_outputs = inputs
+    def call(self, inputs, training=None):
+        token_ids, enc_outputs, look_ahead_mask, padding_mask = inputs
         embeddings = self.embedding(token_ids)
-
-        def _create_mask(x):
-            padding_mask = tf.cast(tf.equal(0, x), dtype=tf.float32)
-            padding_mask = padding_mask[:, tf.newaxis, tf.newaxis, :]
-            size = tf.shape(x)[1]
-            look_ahead_mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
-            look_ahead_mask = tf.maximum(look_ahead_mask, padding_mask)
-            return look_ahead_mask, padding_mask
 
         self_attn_weights, context_attn_weights = [], []
         outputs = embeddings
-        look_ahead_mask, padding_mask = _create_mask(token_ids)
+
         for i in range(self.num_layers):
-            outputs, self_attn, context_attn = self.decoders[i](
-                inputs=(outputs, enc_outputs), look_ahead_mask=look_ahead_mask, padding_mask=padding_mask)
+            decoder = self.decoders[i]
+            outputs, self_attn, context_attn = decoder(inputs=(outputs, enc_outputs, look_ahead_mask, padding_mask))
             self_attn_weights.append(self_attn)
             context_attn_weights.append(context_attn)
 
@@ -307,9 +161,35 @@ class Transformer(tf.keras.layers.Layer):
         self.decoder = TransformerDecoder(config)
         self.dense = tf.keras.layers.Dense(config.target_vocab_size)
 
-    def call(self, inputs):
+    def call(self, inputs, training=None):
         x_ids, y_ids = inputs
-        enc_outputs, enc_attns = self.encoder(x_ids)
-        dec_outputs, dec_attns_0, dec_attns_1 = self.decoder(inputs=(y_ids, enc_outputs))
+
+        def _create_padding_mask(x):
+            mask = tf.cast(tf.equal(0, x), dtype=tf.float32)
+            mask = mask[:, tf.newaxis, tf.newaxis, :]
+            return mask
+
+        def _create_look_ahead_mask(size):
+            mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+            return mask
+
+        def _create_masks(x, y):
+            _enc_padding_mask = _create_padding_mask(x)
+            _dec_padding_mask = _create_padding_mask(x)
+            _look_ahead_mask = _create_look_ahead_mask(tf.shape(y)[1])
+            _target_padding_mask = _create_padding_mask(y)
+            combined = tf.maximum(_look_ahead_mask, _target_padding_mask)
+            return _enc_padding_mask, combined, _dec_padding_mask
+
+        enc_padding_mask, dec_look_ahead_mask, dec_padding_mask = _create_masks(x_ids, y_ids)
+
+        enc_outputs, enc_attns = self.encoder(inputs=(x_ids, enc_padding_mask))
+
+        dec_outputs, dec_attns_0, dec_attns_1 = self.decoder(
+            inputs=(y_ids, enc_outputs, dec_look_ahead_mask, dec_padding_mask))
+
         logits = self.dense(dec_outputs)
-        return logits, enc_attns[-1], dec_attns_0[-1], dec_attns_1[-1]
+        return logits, enc_attns, dec_attns_0, dec_attns_1
+
+    def get_config(self):
+        return super().get_config()
--- transformers_keras/modeling_transformer_test.py
+++ transformers_keras/modeling_transformer_test.py
@@ -1,33 +1,141 @@
-import tensorflow as tf
+import unittest
 
+from transformers_keras.datasets import TransformerTextFileDatasetBuilder
 from transformers_keras.modeling_transformer import *
+from transformers_keras.tokenizers import TransformerDefaultTokenizer
+from .modeling_transformer import Transformer
 
 
-class ModelingTransformerTest(tf.test.TestCase):
-
-    def testTransformerEncoderLayer(self):
-        config = TransformerConfig(hidden_size=512, num_attention_heads=8)
-        encoder_layer = TransformerEncoderLayer(config)
-
-        inputs = [tf.random.uniform((64, 43, config.hidden_size))] * 3
-        output, attn_weights = encoder_layer(inputs=inputs, attn_mask=None)
-
-        self.assertAllEqual(output.shape, [64, 43, config.hidden_size])  # (batch_size, input_seq_len, d_model)
-        # (batch_size, num_heads, seq_length, seq_length)
-        self.assertAllEqual(attn_weights.shape, [64, config.num_attention_heads, 43, 43])
+class ModelingTransformerTest(unittest.TestCase):
 
     def testEncoder(self):
         config = TransformerConfig(num_encoder_layers=2, hidden_size=512, num_attention_heads=8, ffn_size=2048)
         sample_encoder = TransformerEncoder(config)
         inputs = tf.constant([[i for i in range(62)] * 64], shape=(64, 62), dtype=tf.int64)
-        output, attn_weights = sample_encoder(inputs=inputs)
+        output, attn_weights = sample_encoder(inputs=(inputs, None))
 
-        self.assertAllEqual(output.shape, [64, 62, config.hidden_size])  # (batch_size, input_seq_len, d_model)
+        self.assertEqual(output.shape, [64, 62, config.hidden_size])  # (batch_size, input_seq_len, d_model)
 
         # each layers attention weights
         for v in attn_weights:
-            self.assertAllEqual(v.shape, [64, config.num_attention_heads, 62, 62])
+            self.assertEqual(v.shape, [64, config.num_attention_heads, 62, 62])
+
+    def testDecoder(self):
+        config = TransformerConfig(num_decoder_layers=2, hidden_size=512, num_attention_heads=8, ffn_size=2048)
+        sample_decoder = TransformerDecoder(config)
+        y = tf.constant(np.random.randint(0, 10, 43), shape=(1, 43), dtype=tf.int64)
+        enc_outputs = tf.random.uniform((1, 50, config.hidden_size))
+        inputs = (y, enc_outputs, None, None)
+        output, self_attn_weights, context_attn_weights = sample_decoder(inputs=inputs)
+
+        self.assertEqual(output.shape, [1, 43, config.hidden_size])
+        for attn in self_attn_weights:
+            self.assertEqual(attn.shape, [1, config.num_attention_heads, 43, 43])
+        for attn in context_attn_weights:
+            self.assertEqual(attn.shape, [1, config.num_attention_heads, 43, 50])
+
+    def buildTransformerModel(self, config):
+        x = tf.keras.layers.Input(shape=(32,), dtype=tf.int32, name='x')
+        y = tf.keras.layers.Input(shape=(32,), dtype=tf.int32, name='y')
+        model = Transformer(config)
+        logits, _, _, _ = model(inputs=(x, y))
+        probs = tf.keras.layers.Lambda(lambda x: tf.nn.softmax(x), name='probs')(logits)
+
+        model = tf.keras.Model(inputs=[x, y], outputs=[probs])
+
+        model.compile(
+            optimizer='adam',  # TODO(zhouyang.luo) schedule learning rate
+            loss={
+                'probs': tf.keras.losses.SparseCategoricalCrossentropy(name='loss', from_logits=False),
+            },
+            metrics={
+                'probs': [
+                    tf.keras.metrics.SparseCategoricalAccuracy(name='acc'),
+                ]
+            }
+        )
+
+        model.summary()
+        return model
+
+    def buildTokenizers(self):
+        src_tokenizer = TransformerDefaultTokenizer(
+            vocab_file='testdata/vocab_src.txt',
+            pad_token='<pad>', unk_token='<unk>', sos_token='<s>', eos_token='</s>'
+        )
+        tgt_tokenizer = TransformerDefaultTokenizer(
+            vocab_file='testdata/vocab_tgt.txt',
+            pad_token='<pad>', unk_token='<unk>', sos_token='<s>', eos_token='</s>'
+        )
+        return src_tokenizer, tgt_tokenizer
+
+    def testTransformerModel(self):
+        src_tokenizer, tgt_tokenizer = self.buildTokenizers()
+        data_config = {
+            'train_shuffle_buffer_size': 100,
+            'valid_shuffle_buffer_size': 100,
+            'train_repeat_count': 100,
+            'train_batch_size': 2,
+            'valid_batch_size': 2,
+        }
+        dataset_builder = TransformerTextFileDatasetBuilder(src_tokenizer, tgt_tokenizer, **data_config)
+
+        model_config = TransformerConfig(
+            num_encoder_layers=2,
+            num_decoder_layers=2,
+            hidden_siz=512,
+            ffn_size=2048,
+            max_positions=512,
+            source_vocab_size=src_tokenizer.vocab_size,
+            target_vocab_size=tgt_tokenizer.vocab_size,
+        )
+        model = Transformer(model_config)
+        train_dataset = dataset_builder.build_train_dataset(
+            train_files=[('testdata/train.src.txt', 'testdata/train.tgt.txt')])
+        train_dataset = train_dataset.map(lambda x, y: (x[0], x[1]))
+        print(next(iter(train_dataset)))
+
+        for i, v in enumerate(train_dataset):
+            outputs, _, _, _ = model(v)
+
+    def testTraining(self):
+        src_tokenizer = TransformerDefaultTokenizer(
+            vocab_file='testdata/vocab_src.txt',
+            pad_token='<pad>', unk_token='<unk>', sos_token='<s>', eos_token='</s>'
+        )
+        tgt_tokenizer = TransformerDefaultTokenizer(
+            vocab_file='testdata/vocab_tgt.txt',
+            pad_token='<pad>', unk_token='<unk>', sos_token='<s>', eos_token='</s>'
+        )
+        data_config = {
+            'train_shuffle_buffer_size': 100,
+            'valid_shuffle_buffer_size': 100,
+            'train_repeat_count': 100,
+            'train_batch_size': 2,
+            'valid_batch_size': 2,
+        }
+        dataset_builder = TransformerTextFileDatasetBuilder(src_tokenizer, tgt_tokenizer, **data_config)
+        model_config = TransformerConfig(
+            num_encoder_layers=2,
+            num_decoder_layers=2,
+            hidden_siz=512,
+            ffn_size=2048,
+            max_positions=512,
+            source_vocab_size=src_tokenizer.vocab_size,
+            target_vocab_size=tgt_tokenizer.vocab_size,
+        )
+        model = self.buildTransformerModel(model_config)
+        # print(model.get_config())
+        train_dataset = dataset_builder.build_train_dataset(
+            train_files=[('testdata/train.src.txt', 'testdata/train.tgt.txt')])
+        print(next(iter(train_dataset)))
+        valid_dataset = dataset_builder.build_valid_dataset(
+            valid_files=[('testdata/train.src.txt', 'testdata/train.tgt.txt')])
+        model.fit(
+            train_dataset,
+            validation_data=valid_dataset
+        )
 
 
 if __name__ == "__main__":
-    tf.test.main()
+    unittest.main()
