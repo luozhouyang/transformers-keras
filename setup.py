5884eb9e94a72725b2096f883229e096a30271e1
luozhouyang <zhouyang.luo@gmail.com>
2021-01-24 21:33:16 +0800

    Release v0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="transformers_keras",
-    version="0.2.1",
+    version="0.2.2",
     description="Transformer-based models implemented in tensorflow 2.x(Keras)",
     long_description=long_description,
     long_description_content_type="text/markdown",
