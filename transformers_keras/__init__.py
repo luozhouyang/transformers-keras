ca590ac2a3ebb30dfa146fe385e1d8f91afe1e8e
luozhouyang <zhouyang.luo@gmail.com>
2021-07-13 17:13:56 +0800

    Release v0.3.1

--- transformers_keras/__init__.py
+++ transformers_keras/__init__.py
@@ -9,6 +9,6 @@ from .modeling_bert import (Bert, BertEmbedding, BertEncoder, BertEncoderLayer,
 from .modeling_utils import complete_inputs
 
 __name__ = 'transformers_keras'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 logging.basicConfig(format="%(asctime)s %(levelname)7s %(filename)15s %(lineno)4d] %(message)s", level=logging.INFO)
