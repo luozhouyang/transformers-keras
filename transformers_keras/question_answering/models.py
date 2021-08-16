0f7417a119167101f067fbe42d19c79a1791b6f7
luozhuoyang <zhouyang.luo@gmail.com>
2021-08-16 15:11:48 +0800

    Format file

--- transformers_keras/question_answering/models.py
+++ transformers_keras/question_answering/models.py
@@ -2,32 +2,34 @@ import json
 import logging
 
 import tensorflow as tf
-from transformers_keras.modeling_albert import (AlbertModel,
-                                                AlbertPretrainedModel)
+from transformers_keras.modeling_albert import AlbertModel, AlbertPretrainedModel
 from transformers_keras.modeling_bert import BertModel, BertPretrainedModel
 
 
 class BertForQuestionAnswering(BertPretrainedModel):
+    """Bert for question answering"""
 
-    def __init__(self,
-                 num_labels=2,
-                 vocab_size=21128,
-                 max_positions=512,
-                 hidden_size=768,
-                 type_vocab_size=2,
-                 num_layers=6,
-                 num_attention_heads=8,
-                 intermediate_size=3072,
-                 activation='gelu',
-                 hidden_dropout_rate=0.2,
-                 attention_dropout_rate=0.1,
-                 initializer_range=0.02,
-                 epsilon=1e-12,
-                 **kwargs):
+    def __init__(
+        self,
+        num_labels=2,
+        vocab_size=21128,
+        max_positions=512,
+        hidden_size=768,
+        type_vocab_size=2,
+        num_layers=6,
+        num_attention_heads=8,
+        intermediate_size=3072,
+        activation="gelu",
+        hidden_dropout_rate=0.2,
+        attention_dropout_rate=0.1,
+        initializer_range=0.02,
+        epsilon=1e-12,
+        **kwargs
+    ):
         # build functional model
-        input_ids = tf.keras.layers.Input(shape=(None, ), dtype=tf.int32, name='input_ids')
-        segment_ids = tf.keras.layers.Input(shape=(None, ), dtype=tf.int32, name='segment_ids')
-        attention_mask = tf.keras.layers.Input(shape=(None, ), dtype=tf.int32, name='attention_mask')
+        input_ids = tf.keras.layers.Input(shape=(None,), dtype=tf.int32, name="input_ids")
+        segment_ids = tf.keras.layers.Input(shape=(None,), dtype=tf.int32, name="segment_ids")
+        attention_mask = tf.keras.layers.Input(shape=(None,), dtype=tf.int32, name="attention_mask")
         bert_model = BertModel(
             vocab_size=vocab_size,
             max_positions=max_positions,
@@ -41,46 +43,47 @@ class BertForQuestionAnswering(BertPretrainedModel):
             attention_dropout_rate=attention_dropout_rate,
             initializer_range=initializer_range,
             epsilon=epsilon,
-            name='bert')
+            name="bert",
+        )
 
         sequence_output, _, _, _ = bert_model(input_ids, segment_ids, attention_mask)
-        logits = tf.keras.layers.Dense(num_labels, name='dense')(sequence_output)
-        head_logits = tf.keras.layers.Lambda(lambda x: x[:, :, 0], name='head')(logits)
-        tail_logits = tf.keras.layers.Lambda(lambda x: x[:, :, 1], name='tail')(logits)
+        logits = tf.keras.layers.Dense(num_labels, name="dense")(sequence_output)
+        head_logits = tf.keras.layers.Lambda(lambda x: x[:, :, 0], name="head")(logits)
+        tail_logits = tf.keras.layers.Lambda(lambda x: x[:, :, 1], name="tail")(logits)
 
-        super().__init__(
-            inputs=[input_ids, segment_ids, attention_mask],
-            outputs=[head_logits, tail_logits],
-            **kwargs)
+        super().__init__(inputs=[input_ids, segment_ids, attention_mask], outputs=[head_logits, tail_logits], **kwargs)
 
         self.num_labels = num_labels
         self.bert_model = bert_model
 
 
 class AlbertForQuestionAnswering(AlbertPretrainedModel):
+    """Albert for question answering"""
 
-    def __init__(self,
-                 num_labels=2,
-                 vocab_size=21128,
-                 max_positions=512,
-                 embedding_size=128,
-                 type_vocab_size=2,
-                 num_layers=12,
-                 num_groups=1,
-                 num_layers_each_group=1,
-                 hidden_size=768,
-                 num_attention_heads=8,
-                 intermediate_size=3072,
-                 activation='gelu',
-                 hidden_dropout_rate=0.2,
-                 attention_dropout_rate=0.1,
-                 epsilon=1e-12,
-                 initializer_range=0.02,
-                 **kwargs):
+    def __init__(
+        self,
+        num_labels=2,
+        vocab_size=21128,
+        max_positions=512,
+        embedding_size=128,
+        type_vocab_size=2,
+        num_layers=12,
+        num_groups=1,
+        num_layers_each_group=1,
+        hidden_size=768,
+        num_attention_heads=8,
+        intermediate_size=3072,
+        activation="gelu",
+        hidden_dropout_rate=0.2,
+        attention_dropout_rate=0.1,
+        epsilon=1e-12,
+        initializer_range=0.02,
+        **kwargs
+    ):
         # build functional model
-        input_ids = tf.keras.layers.Input(shape=(None, ), dtype=tf.int32, name='input_ids')
-        segment_ids = tf.keras.layers.Input(shape=(None, ), dtype=tf.int32, name='segment_ids')
-        attention_mask = tf.keras.layers.Input(shape=(None, ), dtype=tf.int32, name='attention_mask')
+        input_ids = tf.keras.layers.Input(shape=(None,), dtype=tf.int32, name="input_ids")
+        segment_ids = tf.keras.layers.Input(shape=(None,), dtype=tf.int32, name="segment_ids")
+        attention_mask = tf.keras.layers.Input(shape=(None,), dtype=tf.int32, name="attention_mask")
         albert_model = AlbertModel(
             vocab_size=vocab_size,
             max_positions=max_positions,
@@ -97,17 +100,15 @@ class AlbertForQuestionAnswering(AlbertPretrainedModel):
             attention_dropout_rate=attention_dropout_rate,
             epsilon=epsilon,
             initializer_range=initializer_range,
-            name='albert')
+            name="albert",
+        )
 
         sequence_output, _, _, _ = albert_model(input_ids, segment_ids, attention_mask)
-        logits = tf.keras.layers.Dense(num_labels, name='dense')(sequence_output)
-        head_logits = tf.keras.layers.Lambda(lambda x: x[:, :, 0], name='head')(logits)
-        tail_logits = tf.keras.layers.Lambda(lambda x: x[:, :, 1], name='tail')(logits)
+        logits = tf.keras.layers.Dense(num_labels, name="dense")(sequence_output)
+        head_logits = tf.keras.layers.Lambda(lambda x: x[:, :, 0], name="head")(logits)
+        tail_logits = tf.keras.layers.Lambda(lambda x: x[:, :, 1], name="tail")(logits)
 
-        super().__init__(
-            inputs=[input_ids, segment_ids, attention_mask],
-            outputs=[head_logits, tail_logits],
-            **kwargs)
+        super().__init__(inputs=[input_ids, segment_ids, attention_mask], outputs=[head_logits, tail_logits], **kwargs)
 
         self.num_labels = num_labels
         self.albert_model = albert_model
