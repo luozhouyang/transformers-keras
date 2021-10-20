e825c79b107c7734937e27facf825e6f04dc95cd
luozhouyang <zhouyang.luo@gmail.com>
2021-10-20 17:12:42 +0800

    Release v0.4.9

--- transformers_keras/__init__.py
+++ transformers_keras/__init__.py
@@ -90,6 +90,6 @@ from transformers_keras.token_classification.crf_models import (
 from transformers_keras.token_classification.models import AlbertForTokenClassification, BertForTokenClassification
 
 __name__ = "transformers_keras"
-__version__ = "0.4.8"
+__version__ = "0.4.9"
 
 logging.basicConfig(format="%(asctime)s %(levelname)7s %(filename)20s %(lineno)4d] %(message)s", level=logging.INFO)
