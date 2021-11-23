58b87d5feb5632e3830c2d3b27873df6ae6be4b3
luozhouyang <zhouyang.luo@gmail.com>
2021-11-23 10:34:34 +0800

    Bump version to 0.5.1

--- transformers_keras/__init__.py
+++ transformers_keras/__init__.py
@@ -92,6 +92,6 @@ from transformers_keras.token_classification.crf_models import (
 from transformers_keras.token_classification.models import AlbertForTokenClassification, BertForTokenClassification
 
 __name__ = "transformers_keras"
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 logging.basicConfig(format="%(asctime)s %(levelname)7s %(filename)20s %(lineno)4d] %(message)s", level=logging.INFO)
